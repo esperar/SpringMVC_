6ed280d0c68590b9d5ac5894b8f8aa9206733a81
KIMHUEMANG <piyrw9754@gmail.com>
2022-08-20 19:11:22 +0900

    spring

---
+++ src/main/java/hello/hellospring/HelloSpringApplication.java
@@ -0,0 +1,14 @@
+package hello.hellospring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloSpringApplication {
+
+	public static void main(String[] args) {
+		// 스프링 실행
+		SpringApplication.run(HelloSpringApplication.class, args);
+	}
+
+}
