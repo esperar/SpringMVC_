323bd018635f7cc97a3d3bc4e741a40e6eb5d5fe
KIMHUEMANG <piyrw9754@gmail.com>
2022-08-23 07:58:56 +0900

    Create MemberService test

---
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -0,0 +1,43 @@
+package hello.hellospring.service;
+
+import hello.hellospring.domain.Member;
+import hello.hellospring.repository.MemberRepository;
+import hello.hellospring.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    /*
+    회원가입 기능
+     */
+    public Long join(Member member){
+
+        validateDuplicateMember(member);
+        memberRepository.save(member);
+        return member.getId();
+    }
+    // 중복을 검사하는 기능
+    private void validateDuplicateMember(Member member){
+        memberRepository.findByName(member.getName()).
+                ifPresent(m -> {
+                    throw new IllegalStateException("이미 존재하는 회원입니다.");
+                });
+    }
+
+    /*
+    전체 회원 조회하기
+     */
+    public List<Member> findMember(){
+        return memberRepository.findAll();
+    }
+
+    // 아이디로 회원 조회하기.
+    public Optional<Member> findOne(Long memberId){
+        return memberRepository.findById(memberId);
+    }
+}
+
