b5fc16bf0c21a0e1d2168d319fe9f1e936c33e15
KIMHUEMANG <piyrw9754@gmail.com>
2022-08-22 16:24:33 +0900

    Add helloApi Controller

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -20,4 +21,35 @@ public class HelloController {
         model.addAttribute("name" , name);
         return "hello-template";
     }
+
+    // @ResponseBody 를 사용하면 뷰 리졸버( viewResolver )를 사용하지 않음
+    // 대신에 HTTP 의 BODY 에 문자 내용을 직접 반환
+    @GetMapping("hello-string")
+    @ResponseBody
+    public String helloString(@RequestParam("name") String name){
+        return "hello" + name;
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name){
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+        // @ResponseBody 를 사용하고, 객체를 반환하면 객체가 JSON 으로 변환됨
+    }
+
+    static class Hello{
+        private String name;
+
+        public String getName(){
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+    }
+
 }
