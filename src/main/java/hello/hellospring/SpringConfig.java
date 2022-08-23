7bc9becb69014ae80ba4f74c9c356a3a4d9adfdf
KIMHUEMANG <piyrw9754@gmail.com>
2022-08-24 08:00:47 +0900

    Create SpringConfig

---
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -0,0 +1,22 @@
+package hello.hellospring;
+
+import hello.hellospring.repository.MemberRepository;
+import hello.hellospring.repository.MemoryMemberRepository;
+import hello.hellospring.service.MemberService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringConfig {
+
+    @Bean
+    public MemberService memberService(){
+        return new MemberService(memberRepository());
+    }
+
+    @Bean
+    MemberRepository memberRepository(){
+        return new MemoryMemberRepository();
+    }
+
+}
--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -8,7 +8,6 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.Optional;
 
-@Service
 public class MemberService {
 
     private final MemberRepository memberRepository;
