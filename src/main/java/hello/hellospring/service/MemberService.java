82fe6951bc5dc7fb003fa14f50305f6f95c50da4
KIMHUEMANG <piyrw9754@gmail.com>
2022-08-23 20:55:14 +0900

    Create a MemberServiceTest

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -9,7 +9,11 @@ import java.util.Optional;
 
 public class MemberService {
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final MemberRepository memberRepository;
+
+    public MemberService(MemberRepository memberRepository){
+        this.memberRepository = memberRepository;
+    }
 
     /*
     회원가입 기능
